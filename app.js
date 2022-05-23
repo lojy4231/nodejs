1a49fea0bdfcc34d63c8faba4da986e71d16af2a
lojy4231 <lojy4231@gmail.com>
2022-05-23 20:54:21 +0900

    api 서버 생성

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connect = require("./schemas");
+const app = express();
+const port = 3000;
+
+const postRouter = require("./routes/post");
+
+const requestMiddleware = (req, res, next) => {
+    console.log("Request URL:", req.originalUrl, "-", new Date());
+    next();
+};
+
+app.use(requestMiddleware);
+app.use("/api", [postRouter]);
+
+app.get("/", (req, res) => {
+    res.send("Wellcome lojy's blog")
+});
+
+app.listen(port, () => {
+    console.log(port, "포트로 서버가 켜졌어요")
+});
