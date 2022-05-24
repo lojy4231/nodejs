be207552b655633e7107b2c0a3b25a3c3b69bd2a
lojy4231 <lojy4231@gmail.com>
2022-05-24 15:39:36 +0900

    mongoose로 mongoDB 연결

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const requestMiddleware = (req, res, next) => {
     next();
 };
 
+connect();
+
 app.use(express.json());
 app.use(requestMiddleware);
 
--- routes/posts.js
+++ routes/posts.js
@@ -1,42 +1,47 @@
 const express = require("express");
+const Post = require("../schemas/post");
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.send("this is post page")
 });
 
-const posts = [
-    {
-        postNum: 1,
-        title: "test1",
-        userId: "test1",
-        date: "2022-05-23",
-        content: "test1",
-    },
-    {
-        postNum: 2,
-        title: "test2",
-        userId: "test2",
-        date: "2022-05-23",
-        content: "test2",
-    },
-    {
-        postNum: 3,
-        title: "test3",
-        userId: "test3",
-        date: "2022-05-23",
-        content: "test3",
-    },
-    {
-        postNum: 4,
-        title: "test4",
-        userId: "test4",
-        date: "2022-05-23",
-        content: "tes4",
-    },
-];
+// const posts = [
+//     {
+//         postNum: 1,
+//         title: "test1",
+//         userId: "test1",
+//         date: "2022-05-23",
+//         content: "test1",
+//     },
+//     {
+//         postNum: 2,
+//         title: "test2",
+//         userId: "test2",
+//         date: "2022-05-23",
+//         content: "test2",
+//     },
+//     {
+//         postNum: 3,
+//         title: "test3",
+//         userId: "test3",
+//         date: "2022-05-23",
+//         content: "test3",
+//     },
+//     {
+//         postNum: 4,
+//         title: "test4",
+//         userId: "test4",
+//         date: "2022-05-23",
+//         content: "tes4",
+//     },
+// ];
 
-router.get("/posts", (req, res) => {
+router.get("/posts", async (req, res) => {
+    const { title } = req.query;
+
+    const posts = await Post.find({ title });
+
     res.json({
         posts,
     });
@@ -44,10 +49,23 @@ router.get("/posts", (req, res) => {
 
 router.get("/posts/:postNum", (req, res) => {
     const { postNum } = req.params;
-    const [detail] = posts.filter((posts) => posts.postNum === Number(postNum));
+    const [detail] = Post.find({ postNum: Number(postNum) });
 	res.json({ 
         detail,
     });
+});
+
+router.post("/posting", async (req, res) => {
+    const { postNum, title, userId, password, date, content } = req.body;
+
+    const posts = await Post.find({ postNum });
+    if (posts.length) {
+        return res.status(400).json({ success: false, errorMessage: "이미 있는 데이터 입니다." });
+    }
+
+    const createdPost = await Post.create({ postNum, title, userId, password, date, content });
+
+    res.json({ post: createdPost });
 });
 
 module.exports = router;
